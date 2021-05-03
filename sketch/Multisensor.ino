d678e5c58bb584fcc70343f0a3f7b2966ed7e5dd
smiechowicz <tomasz.smiechowicz@idemia.com>
2021-05-03 19:13:59 +0200

    calibration code refactoring

--- sketch/Multisensor.ino
+++ sketch/Multisensor.ino
@@ -4,6 +4,7 @@
 #include <Adafruit_ADS1X15.h>
 
 #define ONE_WIRE_BUS 0
+#define PH_SENSOR_BUS 2
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
@@ -18,7 +19,7 @@ void setup(void) {
 //interpolation as a linear y=ax+b, a and b are calculated base on liquids 4 and 7, a=(y2-y1)/(x2-x1)
 // more info https://bestengineeringprojects.com/arduino-ph-meter-using-ph-sensor/
 float calibrationB = 21.34;
-float calibrationA = -5.7
+float calibrationA = -5.7;
 
 void loop(void) {
   Serial.println("Requesting temperature...");
@@ -32,14 +33,9 @@ void loop(void) {
   Serial.println(tempC);
 
   Serial.println("Requesting ph...");
-  int16_t sensorValue = ads.readADC_SingleEnded(0);
-  float voltage = ads.computeVolts(sensorValue);
-  Serial.print("Ph sensor value ");
-  Serial.println(sensorValue);
-  Serial.print("Voltage ");
-  Serial.println(voltage);
-  float phValue = calibrationA * voltage + calibrationB;
+  float phValue = calibrationA * ads.computeVolts(ads.readADC_SingleEnded(PH_SENSOR_BUS)) + calibrationB;
   Serial.print("Calculated ph value:  ");
   Serial.println(phValue);
   Serial.println();
+  delay(30000);
 }
