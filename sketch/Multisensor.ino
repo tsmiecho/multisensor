7312f20d98e0fe3809b93b4f84484b2c928a6582
smiechowicz <tomasz.smiechowicz@idemia.com>
2021-05-12 09:43:27 +0200

    Ec meter working

--- sketch/Multisensor.ino
+++ sketch/Multisensor.ino
@@ -2,7 +2,10 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <Adafruit_ADS1X15.h>
+#include <EEPROM.h>
+#include "GravityTDS.h"
 
+#define TdsSensorPin 0
 #define TEMP_SENSOR_BUS 12
 #define PH_SENSOR_ADS_1115_ANALOG_PIN 2
 #define EC_SENSOR_ADS_1115_ANALOG_PIN 3
@@ -10,23 +13,24 @@
 OneWire oneWire(TEMP_SENSOR_BUS);
 DallasTemperature sensors(&oneWire);
 Adafruit_ADS1115 ads;
+GravityTDS gravityTds;
+
+
 
 void setup(void) {
   Serial.begin(115200);
   sensors.begin();
   ads.begin();
+  gravityTds.setPin(TdsSensorPin);
+  gravityTds.setAref(5.0);
+  gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
+  gravityTds.begin();
 }
 
 //interpolation as a linear y=ax+b, a and b are calculated base on liquids 4 and 7, a=(y2-y1)/(x2-x1)
 // more info https://bestengineeringprojects.com/arduino-ph-meter-using-ph-sensor/
 float phCalibrationB = 21.34;
 float phCalibrationA = -5.7;
-
-//more info https://wiki.dfrobot.com/Gravity__Analog_TDS_Sensor___Meter_For_Arduino_SKU__SEN0244
-float k = 0.71; //static value based on electrode
-float ecCalibrationA = 133.42;
-float ecCalibrationB = 255.86;
-float ecCalibrationC = 857.39;
 
 void loop(void) {
   Serial.println("Requesting temperature...");
@@ -38,20 +42,20 @@ void loop(void) {
   Serial.print("Temperature: ");
   Serial.println(tempC);
 
+  delay(1000);
+
   Serial.println("Requesting ph...");
   float phValue = phCalibrationA * ads.computeVolts(ads.readADC_SingleEnded(PH_SENSOR_ADS_1115_ANALOG_PIN)) + phCalibrationB;
   Serial.print("Calculated ph value:  ");
   Serial.println(phValue);
 
-
-
-  Serial.println("Requesting EC...");
+  delay(1000);
 
   //more info https://wiki.dfrobot.com/Gravity__Analog_TDS_Sensor___Meter_For_Arduino_SKU__SEN0244
-  float compensationCoefficient=1.0+0.02*(tempC-25.0);
-  float compensationVoltage=(ads.computeVolts(ads.readADC_SingleEnded(EC_SENSOR_ADS_1115_ANALOG_PIN)))/compensationCoefficient;
-  // EC= (av^3 - bv^2 + cv) * k
-  float ecValue = (ecCalibrationA * compensationVoltage*compensationVoltage*compensationVoltage - ecCalibrationB * compensationVoltage*compensationVoltage + ecCalibrationC * compensationVoltage) * k;
+  Serial.println("Requesting EC...");
+  gravityTds.setTemperature(tempC);
+  gravityTds.update();
+  float ecValue = gravityTds.getEcValue();
   Serial.print("Calculated ec value:  ");
   Serial.println(ecValue);
 
